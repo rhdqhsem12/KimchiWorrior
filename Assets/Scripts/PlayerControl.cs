20af22273d7097689e98846392a4d5a9e6bd4711
knuppe22 <knuppe22@gmail.com>
2017-08-12 01:46:14 +0900

    Basic Interface

---
+++ Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour {
+    public float speed;
+    
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(Input.GetKey("left"))
+        {
+            transform.Translate(Vector2.left * speed);
+        }
+        if (Input.GetKey("right"))
+        {
+            transform.Translate(Vector2.right * speed);
+        }
+    }
+
+    void OnTriggerStay2D(Collider2D other)
+    {
+        Debug.Log("aaa");
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            Debug.Log("Hit");
+            Destroy(other.gameObject);
+        }
+    }
+}
